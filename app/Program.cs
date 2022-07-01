452dd555572ecf8b958d8ccb063a34ac1267f00d
Deepikajayapal <deepikajayapal98@gmail.com>
2022-07-01 12:42:55 +0530

    c sharp code

---
+++ app/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+namespace Hello
+{
+    class Program{
+        static void Main(){
+            // Average();
+            CharacterOccurence();
+        }
+        static void Average(){
+            int number1,number2,number3,average;
+            Console.WriteLine("Enter number1");
+            number1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number2");
+            number2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number3");
+            number3=Convert.ToInt32(Console.ReadLine());
+            average=(number1+number2+number3)/3;
+            Console.WriteLine(average);
+        }
+        static void CharacterOccurence(){
+            string str ="Deepika";
+            Console.WriteLine("Name:"+str);
+            while(str.Length>0){
+                Console.Write(str[0]+"=");
+                int cal=0;
+                for(int i=0;i<str.Length;i++){
+                    if(str[0]==str[i]){
+                        cal++;
+                    }
+                }
+                Console.WriteLine(cal);
+                str=str.Replace(str[0].ToString(),string.Empty);
+            }
+            Console.ReadLine();
+        }
+    }
+}
